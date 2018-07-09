a86aed2fd77846b7be408e798c753a7eda61a915
solangealmn <solangealmn@gmail.com>
2018-07-08 22:01:46 -0300

    Versão 2 - Precisa de ajustes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
-//Pegar o botão pela id
+//postTweet pega o textarea pela id
+var postTweet = document.getElementById('post-tweet');
+//newTweet pega o botão pela id
 var newTweet = document.getElementById('btn-tweet');
+
+//escuta a ação de colocar texto dentro do textarea
+postTweet.addEventListener('keyup', function() {
+  //verifica se tem algum valor dentro de textarea
+  if (postTweet.value.length > 0) {
+    newTweet.disabled = false; //habilita o botão
+  } else{
+    newTweet.disabled = true; //desabilita o botão
+  }
+  var charlimit = 140; //contém o valor limite de caracteres
+  var character = postTweet.value.split(''); //pega cada um dos caracteres digitados
+  remening.innerText = charlimit - character.length; //insere a quantidade de caracteres dentro do p
+});
 //Escuta a ação de enviar um novo tweet.
 newTweet.addEventListener('click', tweet);
-//função tweet guarda o valor de textarea, insere em uma div dentro do html
-function tweet(){
-  //postTweet contém o valor de textarea
-  var postTweet = document.getElementById('post-tweet').value;
-  //post cria uma div vazia
-  var post = document.createElement('div');
-  post.classList = 'printed-tweet';//adiciona uma class a div criada
+
+function tweet() { //função tweet posta o comentário
+  postTweet = document.getElementById('post-tweet').value;
+  var commentParent = document.getElementById('comment-posted');
+  var post = document.createElement('div'); //post cria uma div vazia
+  post.classList = 'printed-tweet'; //adiciona uma class a div criada
   post.innerHTML = postTweet; //insere o valor de textarea dentro da div
-  var commentParent = document.getElementById('comment-posted')
   commentParent.appendChild(post);
+  clean('post-tweet'); //limpa o textarea
   event.preventDefault();
+}
+//limpar o textarea depois de cada tweet
+function clean(clear) {
+  document.getElementById(clear).value = "";
 }
